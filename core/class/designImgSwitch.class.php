1a98b4cafde10abe911b5f1e53e8bad8e952127e
Mips2648 <mips2648@gmail.com>
2020-08-08 09:34:09 +0200

    fix & daytext

--- core/class/designImgSwitch.class.php
+++ core/class/designImgSwitch.class.php
@@ -64,8 +64,23 @@ class designImgSwitch extends eqLogic {
         $listener->addEvent($cmd_sunrise->getId());
         $listener->addEvent($cmd_sunset->getId());
         $listener->save();
+    }
 
-        $this->refreshPlanHeaderBackground();
+    private function createCron(string $timeHi) {
+        $cron = new cron();
+        $cron->setClass(__CLASS__);
+        $cron->setFunction('pullRefresh');
+        $cron->setOption(array('id' => $this->getId()));
+        $cron->setTimeout(3);
+        $cron->setOnce(1);
+
+        $next = str_repeat('0', 4 - strlen($timeHi)) . $timeHi;
+        $next = date('Y-m-d') . ' ' . substr($next, 0, 2) . ':' . substr($next, 2, 4);
+        $next = strtotime($next);
+
+        $cron->setSchedule(cron::convertDateToCron($next));
+        $cron->setLastRun(date('Y-m-d H:i:s'));
+        $cron->save();
     }
 
     public function preInsert() {
@@ -104,6 +119,18 @@ class designImgSwitch extends eqLogic {
             $cmd->setEqLogic_id($this->getId());
             $cmd->save();
         }
+
+        $cmd = $this->getCmd(null, 'daytext');
+        if (!is_object($cmd)) {
+            $cmd = new designImgSwitchCmd();
+            $cmd->setLogicalId('daytext');
+            $cmd->setIsVisible(1);
+            $cmd->setName('Phase du jour');
+            $cmd->setType('info');
+            $cmd->setSubType('string');
+            $cmd->setEqLogic_id($this->getId());
+            $cmd->save();
+        }
     }
 
     public function preUpdate() {
@@ -115,6 +142,7 @@ class designImgSwitch extends eqLogic {
 
     public function postUpdate() {
         $this->setListener();
+        $this->refreshPlanHeaderBackground();
     }
 
     public function preRemove() {
@@ -311,8 +339,12 @@ class designImgSwitch extends eqLogic {
         $hour = date('Hi');
         if ($hour>=$sunrise && $hour < $sunset) {
             $period = "day";
+            $this->checkAndUpdateCmd('daytext', __("Jour" , __FILE__));
+            $this->createCron($sunset);
         } else {
             $period = "night";
+            $this->checkAndUpdateCmd('daytext', __("Nuit" , __FILE__));
+            $this->createCron($sunrise);
         }
         log::add(__CLASS__, 'debug', "day / night ? : {$period}");
         log::add(__CLASS__, 'debug', "condition as text : {$condition}");
